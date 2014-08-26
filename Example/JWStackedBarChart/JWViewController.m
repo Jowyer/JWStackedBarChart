dad007d067adbdb8e7305461a893c217d6409a16
Jowyer <jowyer@gmail.com>
2014-08-26 14:53:00 +0800

    Example update

--- Example/JWStackedBarChart/JWViewController.m
+++ Example/JWStackedBarChart/JWViewController.m
@@ -7,23 +7,32 @@
 //
 
 #import "JWViewController.h"
-
-@interface JWViewController ()
-
-@end
+#import "JWStackedBarChart.h"
 
 @implementation JWViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    // Horizontal
+    JWStackedBarChart *horizontalStackedBar = [[JWStackedBarChart alloc] initWithFrame:CGRectMake(20, 40, 200, 40) IsVertical:NO];
+    [self.view addSubview:horizontalStackedBar];
+    
+    horizontalStackedBar.attributesDic = @{NSForegroundColorAttributeName:[UIColor whiteColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:15]};
+    
+    horizontalStackedBar.segmentsArray = @[[JWBarSegment barSegmentWithValue:20 Color:[UIColor blueColor]], [JWBarSegment barSegmentWithValue:30 Color:[UIColor greenColor]], [JWBarSegment barSegmentWithValue:50 Color:[UIColor redColor]]];
+    [horizontalStackedBar beginDrawing];
+    
+    
+    // Vertical
+    JWStackedBarChart *verticalStackedBar = [[JWStackedBarChart alloc] initWithFrame:CGRectMake(20, 100, 50, 220) IsVertical:YES];
+    [self.view addSubview:verticalStackedBar];
+    
+    verticalStackedBar.attributesDic = @{NSForegroundColorAttributeName:[UIColor whiteColor], NSFontAttributeName:[UIFont boldSystemFontOfSize:15]};
+    
+    verticalStackedBar.segmentsArray = @[[JWBarSegment barSegmentWithValue:15 Color:[UIColor grayColor]], [JWBarSegment barSegmentWithValue:30 Color:[UIColor greenColor]], [JWBarSegment barSegmentWithValue:25 Color:[UIColor blueColor]], [JWBarSegment barSegmentWithValue:30 Color:[UIColor orangeColor]]];
+    [verticalStackedBar beginDrawing];
 }
 
 @end
